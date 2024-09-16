9d9e6b6587d93bc8d0d64bd2295f03a903eb75f3
NEETU MOHAN <mohanneet482@gmail.com>
2024-09-16 14:22:57 +0530

    comit all my changes

---
+++ src/Sept/ex13092024/Lab_16092024.java
@@ -0,0 +1,14 @@
+package Sept.ex13092024;
+
+public class Lab_16092024 {
+
+
+    public static void main(String[] args)
+    {
+        int i = 2;
+        for(i=1 ;i<=10 ; i++)
+        {
+            System.out.printf("%d * %d = %d\n",i,1,i*1);
+        }
+    }
+}
