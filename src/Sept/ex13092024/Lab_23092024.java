718486a1fde610adf155eb33ade660e8bee0b668
NEETU MOHAN <mohanneet482@gmail.com>
2024-09-26 14:10:38 +0530

    comit all my changes

---
+++ src/Sept/ex13092024/Lab_23092024.java
@@ -0,0 +1,11 @@
+package Sept.ex13092024;
+
+public class Lab_23092024 {
+    public static void main(String[] args) {
+        int input = 84;
+        String grade = (input >= 90 )? "A" :  (input >= 80 )? "B" : (input >= 70 )? "C" : "D";
+        System.out.println(grade);
+
+
+    }
+}
